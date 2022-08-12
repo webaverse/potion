1bbac3197c8cb2b13be11ad0bfa1542c4daa8f80
memelotsqui <memelotsqui@gmail.com>
2022-08-12 09:36:32 -0500

    update in index.js code

--- index.js
+++ index.js
@@ -1,85 +1,92 @@
 import * as THREE from 'three';
 // import easing from './easing.js';
 import metaversefile from 'metaversefile';
+import { Quaternion, Vector3 } from 'three';
 const {useApp, useFrame, useActivate, useWear, useLoaders, usePhysics, useCleanup, useLocalPlayer, useScene, useInternals} = metaversefile;
 
 const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
 
+const vec3Position = new Vector3();
+const vec3Scale = new Vector3();
+const quatRotation = new Quaternion();
 
-
+const _getWorldTransform = (obj) =>{
+    obj.getWorldPosition(vec3Position);
+    obj.getWorldScale(vec3Scale);
+    obj.getWorldQuaternion(quatRotation);
+}
 
 export default e => {
     const app = useApp();
-    const scene = useScene();
+    const physics = usePhysics();
+    const localPlayer = useLocalPlayer();
 
     app.name = "potion";
+    app.content = 12;
+
+    let fluid = null;
+    let potionApp = null;
+    let wearing = false;
+    let active = true;
+    
+    _getWorldTransform(app);
+    const physicsObject = physics.addCapsuleGeometry(
+        vec3Position,
+        quatRotation,
+        0.2,
+        0
+    );
+
 
     e.waitUntil((async () => {
-        let u = `${baseUrl}potion.glb`;
+        let u = `${baseUrl}potion_2.glb`;
         if (/^https?:/.test(u)) {
             u = '/@proxy/' + u;
         }
-        //const m = await metaversefile.import(u);
-        const potionApp = await metaversefile.createAppAsync({
+        potionApp = await metaversefile.createAppAsync({
             start_url:u, 
-            components: app.components,
-            position: app.position,
-            quaternion: app.quaternion,
-            scale: app.scale,
-            parent: app.parent
         });
-        potionApp.instanceId = "CHANGETOGETID5"//
-        scene.add(potionApp);
-        
-        console.log(potionApp)
-        console.log(app)
-        //await potionApp.addModule(m);
-        //console.log(potionApp)
+        app.add(potionApp.glb.scene);
+        potionApp.glb.scene.traverse( (o) => {
+            if (fluid === null && o.name.includes("fluid"))fluid = o;
+        });
     })());
-    // if (/^https?:/.test(u2)) {
-    //   u2 = '/@proxy/' + u2;
-    // }
-    // const m = await metaversefile.import(u2);
-    // // console.log('group objects 3', u2, m);
-    // explosionApp = metaversefile.createApp({
-    //   name: u2,
-    // });
-    // explosionApp.contentId = u2;
-    // explosionApp.instanceId = getNextInstanceId();
-    // explosionApp.position.copy(app.position);
-    // explosionApp.quaternion.copy(app.quaternion);
-    // explosionApp.scale.copy(app.scale);
-    // explosionApp.updateMatrixWorld();
-    // explosionApp.name = 'explosion';
-    // subApps[0] = explosionApp;
-
-    // await explosionApp.addModule(m);
-    // scene.add(explosionApp);
-    // explosionApp.add( bulletPointLight );
-
-    const _loadGlb = async url => {
-        let o = await new Promise((accept, reject) => {
-            const {gltfLoader} = useLoaders();
-            gltfLoader.load(`${baseUrl}${url}`, accept, function onprogress() {}, reject);
-        });
-        return o;
-    };
 
 
-    (async () => {
-        //console.log("loads222")
-        //const potionObject = await _loadGlb("potion.glb");
-        
-        //const potion = potionObject.scene;
-        //app.add(potion);
-        //potion.updateMatrixWorld();
+    const drinkPotion = (timeDiff) =>{
+        app.content -= timeDiff * 0.01;
 
-        //potion.traverse((o)=>{
-            //console.log(o)
-        //})
-        //console.log (physics);
-        //console.log(potion);
-    })();
+        if (app.content < 0){
+            const newCureAction = {
+                type: 'cure'
+            };
+            localPlayer.addAction(newCureAction);
+            
+            
+            active = false;
+            app.content = 0;
+            console.log(app.removeComponent)
+            app.removeComponent("use");
+            app.name = "Empty Bottle";
+            if (fluid) fluid.visible = false;
+
+            console.log(app.components);
+        }
+        if (fluid) 
+            fluid.scale.set(1,app.content/12,1);
+    }
+
+    useFrame(({timeDiff})=>{
+        if (wearing && localPlayer.getAction('use') && active)
+            drinkPotion(timeDiff);
+    });
+
+    
+    useWear(e => {
+      wearing = e.wear;
+    });
+
+    
     return app;
 
 }

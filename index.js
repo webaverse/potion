764d8df70b601ec5d05daa1609ba95bb21436738
memelotsqui <memelotsqui@gmail.com>
2022-08-12 21:04:24 -0500

    finish base code and comment for better information

--- index.js
+++ index.js
@@ -1,87 +1,104 @@
-import * as THREE from 'three';
-// import easing from './easing.js';
 import metaversefile from 'metaversefile';
-import { Quaternion, Vector3 } from 'three';
-const {useApp, useFrame, useActivate, useWear, useLoaders, usePhysics, useCleanup, useLocalPlayer, useScene, useInternals} = metaversefile;
+
+const {useApp, useFrame, useWear, usePhysics, useCleanup, useLocalPlayer} = metaversefile;
 
 const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
-
-const vec3Position = new Vector3();
-const vec3Scale = new Vector3();
-const quatRotation = new Quaternion();
-
-const _getWorldTransform = (obj) =>{
-    obj.getWorldPosition(vec3Position);
-    obj.getWorldScale(vec3Scale);
-    obj.getWorldQuaternion(quatRotation);
-}
 
 export default e => {
     const app = useApp();
     const physics = usePhysics();
     const localPlayer = useLocalPlayer();
 
-    app.name = "potion";
-    app.content = 12;
+    const potionData = app.getComponent("potion") || {};
+    const{fluidName, potionName, emptyName, effect, stats } = potionData;
 
-    let fluid = null;
-    let potionApp = null;
-    let wearing = false;
-    let active = true;
+    app.name = potionName || "Potion";
     
-    _getWorldTransform(app);
-    const physicsObject = physics.addCapsuleGeometry(
-        vec3Position,
-        quatRotation,
-        0.2,
-        0
-    );
+    let triggerTime,initialTime = 1.2;  // In seconds / save twice to normalize later
+    let fluid = null;                   // The 3d model of the fluid inside the gltf file
+    let potionApp = null;               // The subapp that holds the model information
+    let wearing = false;
 
-
+    // Load 3d model of potion
     e.waitUntil((async () => {
-        let u = `${baseUrl}potion_2.glb`;
+        let u = `${baseUrl}potion.glb`;
         if (/^https?:/.test(u)) {
             u = '/@proxy/' + u;
         }
         potionApp = await metaversefile.createAppAsync({
             start_url:u, 
         });
-        app.add(potionApp.glb.scene);
-        potionApp.glb.scene.traverse( (o) => {
-            if (fluid === null && o.name.includes("fluid"))fluid = o;
+
+        // Once loaded, dont forget to add it to your main App to be displayed in the scene
+        app.add(potionApp);
+
+        // Use the imported model to create a geometry physics.
+        physics.addGeometry(potionApp);
+
+        // Find an object in the imported model hierarchy with the name "fluid" or the fluidName set by user in the components
+        potionApp.traverse( (o) => {
+            if (fluid === null && o.name.includes((fluidName||"fluid"))){
+                fluid = o;
+            }
         });
     })());
 
+    const _drinkPotion = (timeDiff) =>{
+        // TimeDiff is in miliseconds, multiply it by 0.001 to get it in seconds
+        triggerTime -= timeDiff * 0.001;
 
-    const drinkPotion = (timeDiff) =>{
-        app.content -= timeDiff * 0.01;
+        if (triggerTime < 0){
 
-        if (app.content < 0){
-            const newCureAction = {
-                type: 'cure'
-            };
-            localPlayer.addAction(newCureAction);
+            _triggerPotionEffect(effect || 'cure');
+            triggerTime = 0;
             
-            
-            active = false;
-            app.content = 0;
-            console.log(app.removeComponent)
-            app.removeComponent("use");
-            app.name = "Empty Bottle";
-            if (fluid) fluid.visible = false;
-
-            console.log(app.components);
         }
+        _setFluidScale(triggerTime/initialTime);
+    }
+
+    // Set the fluid scale depending on the time user has used the potion
+    const _setFluidScale = (scale) => {
+
+        // Set it only in y value, in your 3d model application make sure the pivot of the fluid is in the bottom of the fluid model
         if (fluid) 
-            fluid.scale.set(1,app.content/12,1);
+            fluid.scale.set(1,scale,1);
+    }
+
+    // The actual trigger, here is what happens when the user finish triggering the potion
+    const _triggerPotionEffect = (useEffect) =>{
+
+        // Visual effects of the potion
+        const effectAction = {
+            type: useEffect
+        };
+        localPlayer.addAction(effectAction);
+
+        // TODO: localPlayer apply "stats"
+
+        // Disable the ability to use the potion
+        // In this case remove "drink" behaviour
+        app.removeComponent("use"); 
+        // TODO: instead of removing the use action, change it from "drink" to "swing" (to fill the bottle again)
+        app.name = emptyName || "Empty Bottle";
+        if (fluid) fluid.visible = false;
     }
 
     useFrame(({timeDiff})=>{
-        if (wearing && localPlayer.getAction('use') && active)
-            drinkPotion(timeDiff);
+        // Action "use" is valid everytime the user is clicking while wearing in hand anything that can be used, so make sure you're also validating user is wearing this app
+        if (wearing && localPlayer.getAction('use')){
+            
+            _drinkPotion(timeDiff);
+        }
+        // If user does not click for at least the trigger time, reset to original state of the fluid and the triggerTime counter
+        else{
+            if (triggerTime != initialTime) {
+                triggerTime = initialTime;
+                _setFluidScale(1);
+            }
+        }         
     });
-
     
+    // Detects when the user is holding this item
     useWear(e => {
       wearing = e.wear;
     });
